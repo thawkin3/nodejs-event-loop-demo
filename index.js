b84470dd6b7bd0c8e1a22201157bb3dc93eee2bf
Tyler Hawkins <thawkin3@byu.net>
2021-04-17 14:18:18 -0600

    feat: adds three examples

--- index.js
+++ index.js
@@ -7,19 +7,97 @@ express()
   .get('/', (req, res) => {
     res.sendFile('index.html', { root: 'public' })
   })
+  // Runs in this order:
+  //   foo
+  //   bar
+  //   baz
   .get('/example1', (req, res) => {
-    res.json({
-      demo: 'example1'
-    })
+    const messages = [];
+
+    const foo = () => messages.push('foo')
+    const bar = () => messages.push('bar')
+    const baz = () => messages.push('baz')
+
+    foo()
+    bar()
+    baz()
+
+    // Really hacky way to create a poll waiting for all the
+    // messages to be sent without introducing more promises
+    // or callbacks directly into the examples.
+    const sendResponse = () => {
+      if (messages.length === 3) {
+        res.json({
+          demo: 'Example 1',
+          messages,
+        })
+      } else {
+        setTimeout(sendResponse, 250)
+      }
+    }
+
+    sendResponse()
   })
+  // Runs in this order:
+  //   foo
+  //   baz
+  //   bar
   .get('/example2', (req, res) => {
-    res.json({
-      demo: 'example2'
-    })
+    const messages = [];
+
+    const foo = () => messages.push('foo')
+    const bar = () => messages.push('bar')
+    const baz = () => messages.push('baz')
+
+    foo()
+    setTimeout(bar, 0)
+    baz()
+
+    // Really hacky way to create a poll waiting for all the
+    // messages to be sent without introducing more promises
+    // or callbacks directly into the examples.
+    const sendResponse = () => {
+      if (messages.length === 3) {
+        res.json({
+          demo: 'Example 2',
+          messages,
+        })
+      } else {
+        setTimeout(sendResponse, 250)
+      }
+    }
+
+    sendResponse()
   })
+  // Runs in this order:
+  //   foo
+  //   baz
+  //   bar
   .get('/example3', (req, res) => {
-    res.json({
-      demo: 'example3'
-    })
+    const messages = [];
+
+    const foo = () => messages.push('foo')
+    const bar = () => messages.push('bar')
+    const baz = () => messages.push('baz')
+
+    foo()
+    setTimeout(bar, 0)
+    setImmediate(baz)
+
+    // Really hacky way to create a poll waiting for all the
+    // messages to be sent without introducing more promises
+    // or callbacks directly into the examples.
+    const sendResponse = () => {
+      if (messages.length === 3) {
+        res.json({
+          demo: 'Example 3',
+          messages,
+        })
+      } else {
+        setTimeout(sendResponse, 250)
+      }
+    }
+
+    sendResponse()
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
